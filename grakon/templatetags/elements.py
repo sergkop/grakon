a5cca69b65bbbf3bcd1902408e676da58bd345ec
Sergey Kopylov <sergkop@gmail.com>
2012-04-05 20:31:57 +0100

    new tabs implementation

--- grakon/templatetags/elements.py
+++ grakon/templatetags/elements.py
@@ -18,48 +18,28 @@ def button(icon, title, id=None, link='', center='center', tip=''):
 @register.tag(name="tabs")
 def tabs_tag(parser, token):
     args = token.split_contents()
-    if len(args) % 4 != 1:
-        raise template.TemplateSyntaxError("Incorrect number of arguments")
+    if len(args) != 4:
+        raise template.TemplateSyntaxError("tabs tag takes exactly 3 arguments")
     return TabsNode(*args[1:])
 
 class TabsNode(template.Node):
     def __init__(self, *args):
         """
-        args is a sequence of 'id', 'active_url', 'param_name', 'param_value' and tripples (title, url, include_path, css_class) (a 1-dimensional list).
-        All urls must either start with '/' or be view names.
-        param_name and param_value serve to pass extra parameter to the view.
+        args is a sequence of id, tabs, active_tab.
+        tabs is a dict with keys 'name', 'title', 'url', 'template', 'css_class'.
         """
         self.args = [template.Variable(arg) for arg in args]
 
     def render(self, context):
-        self.args = [arg.resolve(context) for arg in self.args]
-        id = self.args[0]
-        active_url_or_template = self.args[1]
-        param_name = self.args[2]
-        param_value = self.args[3]
-        args = self.args[4:]
+        id, tabs, active = self.args = [arg.resolve(context) for arg in self.args]
 
-        tabs = []
-        num = len(args) / 4
-        for i in range(num):
-            url = args[4*i+1]
-            if url[0] != '/':
-                context_dict = context.dicts[0] # a hack to extract context dict from RequestContext
-                kwargs = {}
-                if param_name in context_dict:
-                    kwargs[param_name] = param_value
-                url = reverse(url, kwargs=kwargs)
+        active_tabs = filter(lambda tab: tab['name']==active, tabs)
+        assert len(active_tabs)==1, "Active tab must be uniquely identified"
 
-            tabs.append({
-                'title': args[4*i],
-                'url': url,
-                'path': args[4*i+2],
-                'css_class': args[4*i+3],
-                'active': active_url_or_template==args[4*i+1],
-            })
-
-        active_tabs = filter(lambda tab: tab['active'], tabs)
-        tab_path = active_tabs[0]['path'] if active_tabs else active_url_or_template
-
-        context.update({'id': id, 'tabs': tabs, 'tab_path': tab_path})
+        context.update({
+            'id': id,
+            'tabs': tabs,
+            'template_path': active_tabs[0]['template'],
+            'active': active
+        })
         return render_to_string('elements/tabs.html', context)
--- navigation/urls.py
+++ navigation/urls.py
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 from django.conf.urls.defaults import patterns, url
 
 def static_url(name, template, tabs=[]):
@@ -5,18 +6,22 @@ def static_url(name, template, tabs=[]):
             {'tab': name, 'template': template, 'tabs': tabs}, name=name)
 
 def static_tabs_urls(base_template, tabs_short):
-    """ tabs_short=[(name, template, css_class), ...] """
-    tabs = [(name, '/'+name, template, css_class)
-            for name, template, css_class in tabs_short]
+    """
+    tabs_short=[(name, title, temsplate, css_class), ...]
+    name here coincides with url slug.
+    """
+    tabs = [{'name': name, 'title': title, 'url': '/'+name,
+            'template': template, 'css_class': css_class}
+            for name, title, template, css_class in tabs_short]
 
-    return patterns('navigation.views',
-            *[static_url(name, base_template, tabs) \
-            for name, template, css_class in tabs_short])
+    return [static_url(name, base_template, tabs)
+            for name, title, template, css_class in tabs_short]
 
 urlpatterns = patterns('navigation.views',
     url(r'^$', 'main', name='main'),
-)
 
-urlpatterns += static_tabs_urls('static_pages/about/base.html', [
-    ('about', 'static_pages/about/about.html', ''),
-])
+    *static_tabs_urls('static_pages/about/base.html', [
+        ('about', u'Описание', 'static_pages/about/about.html', ''),
+        ('publications', u'О нас в СМИ', 'static_pages/about/publications.html', ''),
+    ])
+)
