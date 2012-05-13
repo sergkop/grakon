27ea98d35af7b6f174975b062e606a96db22d5dc
Sergey Kopylov <sergkop@gmail.com>
2012-05-13 07:17:17 +0100

    datetime widget

---
+++ elements/widgets.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+from django.forms.widgets import DateTimeInput
+from django.utils.safestring import mark_safe
+
+# TODO: compress and merge media files
+class DateTimeWidget(DateTimeInput):
+    class Media:
+        css = {
+            'all': ('libs/timepicker/timepicker.css',)
+        }
+        js = ('libs/timepicker/timepicker.js',)
+
+    def render(self, name, value, attrs=None):
+        html = super(DateTimeWidget, self).render(name, value, attrs)
+        html += '<script type="text/javascript">' \
+                    '$(function(){' \
+                        'var datetime_input = $("#id_%(name)s");' \
+                        'datetime_input.datetimepicker({' \
+                            'stepMinute: 10,' \
+                            u'timeText: "Время",' \
+                            u'hourText: "Часы",' \
+                            u'minuteText: "Минуты",' \
+                            u'currentText: "Сейчас",' \
+                            u'closeText: "Закрыть",' \
+                            'dateFormat: "dd/mm/yy",' \
+                            'timeFormat: "hh:mm",' \
+                            'ampm: false' \
+                        '});' \
+                    '});' \
+                '</script>' % {'name': name}
+        return mark_safe(html)
--- tools/events/forms.py
+++ tools/events/forms.py
@@ -5,10 +5,13 @@ from crispy_forms.layout import Fieldset, Layout
 
 from elements.locations.forms import location_init
 from elements.utils import form_helper
+from elements.widgets import DateTimeWidget
 from tools.events.models import Event
 
 @location_init(False, u'Место проведения')
 class EventForm(forms.ModelForm):
+    event_time = forms.DateTimeField(input_formats=['%d/%m/%Y %H:%M'], widget=DateTimeWidget(format='%d/%m/%Y %H:%M'))
+
     class Meta:
         model = Event
 
