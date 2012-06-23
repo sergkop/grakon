cd6d934cf6238c55d598bf462dac5683dd34077c
Sergey Kopylov <sergkop@gmail.com>
2012-06-23 18:54:04 +0100

    bug fix

--- grakon/middleware.py
+++ grakon/middleware.py
@@ -4,4 +4,4 @@ class MinifyHTMLMiddleware(object):
     def process_response(self, request, response):
         if 'text/html' in response['Content-Type']:
             response.content = strip_spaces_between_tags(response.content)
-            return response
+        return response
